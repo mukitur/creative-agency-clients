bbdebadcfd0a07dbb173156dd404f16852a34ffb
mukitur <mukit.inway@gmail.com>
2021-11-22 08:47:05 +0600

    component set and mUI install

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Contact from './Pages/Contact/Contact';
+import Home from './Pages/Home/Home/Home';
+import OurPortfolio from './Pages/OurPortfolio/OurPortfolio';
 
 function App() {
   return (
     <div className="App">
-      
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />}/>
+            <Route path="/home" element={<Home />}/>
+            <Route path="/portfolio" element={<OurPortfolio />}/>
+            <Route path="/contact" element={<Contact />}/>
+          </Routes>
+        
+        </BrowserRouter>
     </div>
   );
 }
---
+++ src/Pages/Home/OurTeam/OurTeam.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const OurTeam = () => {
+    return (
+        <div>
+            <h2>Our Team</h2>
+        </div>
+    );
+};
+
+export default OurTeam;
